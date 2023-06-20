3c756f22adbd1afb34f27cd034f53d3f98226810
Hubertoom <h.michalec@o2.pl>
2023-06-20 08:57:39 +0200

    stage_2 done

--- readability/Main.java
+++ readability/Main.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(scanner.nextLine().length() > 100 ? "HARD" : "EASY");
+        String input = scanner.nextLine();
+        double numberOfWords = input.split("\\s").length;
+        double numberOfSentences = input.split("[.?!]").length;
+
+        System.out.println(numberOfWords / numberOfSentences > 10 ? "HARD" : "EASY");
     }
 }

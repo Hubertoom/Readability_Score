ea40592d81cd73b0bd992cf1e8d10c0351e29ab2
Hubertoom <h.michalec@o2.pl>
2023-06-20 12:55:47 +0200

    adding input.txt file to store text for evaluation

--- readability/Main.java
+++ readability/Main.java
@@ -5,8 +5,7 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) {
-        File file = new File("in.txt");
-
+        File file = new File("input.txt");
         new ReadabilityScoreApp().runApp(file);
     }
 }
